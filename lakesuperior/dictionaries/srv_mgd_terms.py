28e4f3b189a74ef2b9baf965c2a2d76bc6731f48
Stefano Cossu <scossu@artic.edu>
2017-10-31 17:02:10 -0500

    Add server managed terms.

---
+++ lakesuperior/dictionaries/srv_mgd_terms.py
@@ -0,0 +1,35 @@
+from lakesuperior.dictionaries.namespaces import ns_collection as nsc
+
+srv_mgd_subjects = {
+    nsc['fcsystem'].root,
+}
+
+srv_mgd_predicates = {
+    nsc['fcrepo'].created,
+    nsc['fcrepo'].createdBy,
+    nsc['fcrepo'].hasFixityService,
+    nsc['fcrepo'].hasParent,
+    nsc['fcrepo'].lastModified,
+    nsc['fcrepo'].lastModifiedBy,
+    nsc['fcrepo'].writable,
+    nsc['iana'].describedBy,
+    nsc['ldp'].contains,
+    nsc['premis'].hasMessageDigest,
+    nsc['premis'].hasSize,
+}
+
+srv_mgd_types = {
+    nsc['fcrepo'].Binary,
+    nsc['fcrepo'].Container,
+    nsc['fcrepo'].Pairtree,
+    nsc['fcrepo'].Resource,
+    nsc['ldp'].BasicContainer,
+    nsc['ldp'].Container,
+    nsc['ldp'].DirectContainer,
+    nsc['ldp'].IndirectContainer,
+    nsc['ldp'].NonRDFSource,
+    nsc['ldp'].RDFSource,
+    nsc['ldp'].Resource,
+}
+
+
