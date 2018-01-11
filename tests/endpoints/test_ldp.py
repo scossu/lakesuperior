828dce516d05a3c1245bee92d03add4eefdd3487
Stefano Cossu <scossu@artic.edu>
2018-01-10 22:16:04 -0600

    Update TODO.

--- tests/endpoints/test_ldp.py
+++ tests/endpoints/test_ldp.py
@@ -281,17 +281,17 @@ class TestLdp:
         # @TODO Leave commented until a decision is made about SSR.
         #assert self.client.patch(
         #    path, data=nossr_qry,
-        #    headers={'content-type' : 'application/sparql-update'}
+        #    headers={'content-type': 'application/sparql-update'}
         #).status_code == 412
 
         assert self.client.patch(
             path, data=abs_qry,
-            headers={'content-type' : 'application/sparql-update'}
+            headers={'content-type': 'application/sparql-update'}
         ).status_code == 204
 
         assert self.client.patch(
             path, data=frag_qry,
-            headers={'content-type' : 'application/sparql-update'}
+            headers={'content-type': 'application/sparql-update'}
         ).status_code == 204
 
 
