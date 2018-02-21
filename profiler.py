854b6e42962a92b83ddec2970c1bfd65efe2d354
Stefano Cossu <scossu@artic.edu>
2018-02-20 21:53:34 -0600

    Update performance doc.

--- profiler.py
+++ profiler.py
@@ -10,7 +10,7 @@ fcrepo = create_app(config['application'], config['logging'])
 
 options = {
     'restrictions': [30],
-    'profile_dir': '/tmp/lsup_profiling'
+    #'profile_dir': '/tmp/lsup_profiling'
 }
 fcrepo.wsgi_app = ProfilerMiddleware(fcrepo.wsgi_app, **options)
 fcrepo.config['PROFILE'] = True
