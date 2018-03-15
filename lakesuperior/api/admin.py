be861b412803fb44a6fb94251f0449aaaca50139
Stefano Cossu <scossu@artic.edu>
2018-03-14 23:00:52 -0500

    WIP migration tool.

--- lakesuperior/api/admin.py
+++ lakesuperior/api/admin.py
@@ -1,5 +1,7 @@
 import logging
 
+import lmdb
+
 from lakesuperior.env import env
 from lakesuperior.store.ldp_rs.lmdb_store import TxnManager
 
@@ -25,3 +27,34 @@ def stats():
 
     return repo_stats
 
+
+def dump(src, dest, start='/', binary_handling='include'):
+    '''
+    Dump a whole LDP repository or parts of it to disk.
+
+    @param src (rdflib.term.URIRef) Webroot of source repository. This must
+    correspond to the LDP root node (for Fedora it can be e.g.
+    `http://localhost:8080fcrepo/rest/`) and is used to determine if URIs
+    retrieved are managed by this repository.
+    @param dest (rdflib.URIRef) Base URI of the destination. This can be any
+    container in a LAKEsuperior server. If the resource exists, it must be an
+    LDP container. If it does not exist, it will be created.
+    @param start (tuple|list) List of starting points to retrieve resources
+    from. It would typically be the repository root in case of a full dump
+    or one or more resources in the repository for a partial one.
+    @binary_handling (string) One of 'include', 'truncate' or 'split'.
+    '''
+    # 1. Retrieve list of resources.
+    if not isinstance(start, list) and not isinstance(start, tuple):
+        start = (start,)
+    subjects = _gather_subjects(src, start)
+
+
+def _gather_subjects(webroot, start_pts):
+    env = lmdb.open('/var/tmp/
+    for start in start_pts:
+        if not start.startswith('/'):
+            raise ValueError('Starting point {} does not begin with a slash.'
+                    .format(start))
+
+        pfx = src.rstrip('/') + start
