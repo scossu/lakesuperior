ad15bb8cc3dcc6c7af24909cec76c959697d96cf
Stefano Cossu <scossu@artic.edu>
2018-03-25 21:10:33 -0500

    Docstring conversion #2
    * Query API
    * Resource API

--- lakesuperior/api/query.py
+++ lakesuperior/api/query.py
@@ -14,17 +14,18 @@ rdf_store = env.app_globals.rdf_store
 
 
 def sparql_query(qry_str, fmt):
-    '''
+    """
     Send a SPARQL query to the triplestore.
 
-    @param qry_str (str) SPARQL query string. SPARQL 1.1 Query Language
+    :param str qry_str: SPARQL query string. SPARQL 1.1 Query Language
     (https://www.w3.org/TR/sparql11-query/) is supported.
-    @param fmt(string) Serialization format. This varies depending on the
+    :param str fmt: Serialization format. This varies depending on the
     query type (SELECT, ASK, CONSTRUCT, etc.). [@TODO Add reference to RDFLib
     serialization formats]
 
-    @return BytesIO
-    '''
+    :rtype: BytesIO
+    :return: Serialized SPARQL results.
+    """
     with TxnManager(rdf_store) as txn:
         qres = rdfly.raw_query(qry_str)
         out_stream = BytesIO(qres.serialize(format=fmt))
