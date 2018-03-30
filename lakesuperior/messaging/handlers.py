279873e9104fa5c00615d7efe5ccab1d58875570
Stefano Cossu <scossu@artic.edu>
2018-03-30 16:03:49 -0500

    Experimental: do not abort startup if cannot connect to STOMP server.

--- lakesuperior/messaging/handlers.py
+++ lakesuperior/messaging/handlers.py
@@ -3,6 +3,9 @@ import logging
 import stomp
 
 
+logger = logging.getLogger(__name__)
+
+
 class StompHandler(logging.Handler):
     """
     Send messages to a remote queue broker using the STOMP protocol.
@@ -22,21 +25,30 @@ class StompHandler(logging.Handler):
 
         self.conn = conn_cls([(self.conf['host'], self.conf['port'])])
         self.conn.start()
-        self.conn.connect(
-            username=self.conf['username'],
-            passcode=self.conf['password'],
-            wait=True
-        )
+        try:
+            self.conn.connect(
+                username=self.conf['username'],
+                passcode=self.conf['password'],
+                wait=True
+            )
+        except stomp.exception.ConnectFailedException:
+            logger.warning(
+                    'Could not connect to the STOMP server. Your messages '
+                    'will be ditched.')
 
         return super().__init__()
 
 
     def __del_(self):
         """Disconnect the client."""
-        self.conn.disconnect()
+        if self.conn.is_connected():
+            self.conn.disconnect()
 
     def emit(self, record):
         """Send the message to the destination endpoint."""
-        self.conn.send(destination=self.conf['destination'],
-                body=bytes(self.format(record), 'utf-8'))
+        if self.conn.is_connected():
+            self.conn.send(destination=self.conf['destination'],
+                    body=bytes(self.format(record), 'utf-8'))
+        else:
+            logger.warning('STOMP server not connected. Message dropped.')
 
