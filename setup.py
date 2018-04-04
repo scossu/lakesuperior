9302eb54ededed2df476151e3523a0b79af7c736
Stefano Cossu <4306733+scossu@users.noreply.github.com>
2018-04-03 23:08:07 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='lakesuperior',
-    version='1.0.0a99',
+    version='1.0.0a10',
 
     description='A Linked Data Platform repository sever.',
     long_description=long_description,
